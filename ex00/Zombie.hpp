fc71a50933c8f27bea9464b51958bcc86764bcae
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-01-15 19:17:25 +0100

    adding ex02

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -1,14 +1,15 @@
-#include <string>
 #include <iostream>
+#include <string>
 class Zombie
 {
-	private:
-		std::string name;
-	public:
-		Zombie();
-		Zombie(std::string name);
-		Zombie* newZombie( std::string name );
-		void	randomChump( std::string name );
-		void	announce( void );
-		~Zombie();
+private:
+  std::string name;
+
+public:
+  Zombie();
+  Zombie(std::string name);
+  Zombie *newZombie(std::string name);
+  void randomChump(std::string name);
+  void announce(void);
+  ~Zombie();
 };
