2fd64738efce1e4e04878127098c026725b47f4f
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-01-15 18:12:24 +0100

    fixed some header issues

--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -1,3 +1,7 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
 #include <string>
 class Zombie
 {
@@ -11,3 +15,4 @@ public:
   Zombie *zombieHorde(int N, std::string name);
   ~Zombie();
 };
+#endif
