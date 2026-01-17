6d00f2d11abd8fb8faaa0cde4dcf5ddf4c6360d1
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-01-17 18:18:47 +0100

    finished ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -8,15 +8,35 @@ int main (int argc, char **argv)
 		std::cout << "Unexpected amount of arguments" << std::endl;
 		return (1);
 	}
+	std::string outfile_name(argv[1]);
+	outfile_name += ".replace";
 	std::string oldnew[2];
+	std::string buff;
 	oldnew[0] = argv[2];
 	oldnew[1] = argv[3];
-	std::ifstream input(argv[1]);
+	std::ifstream input(argv[1], std::ios_base::in);
 	if (input.fail())
 	{
-		std::cout << "read fail" << std::endl;
+		std::cout << "in file fail" << std::endl;
 		return (1);
 	}
-	while(getline(input))
-	
+	std::ofstream output (outfile_name.c_str(), std::ios_base::out);
+	if (output.fail())
+	{
+		std::cout << "out file fail" << std::endl;
+		return (1);
+	}
+	while(getline(input, buff))
+	{
+		ssize_t pos = buff.find(oldnew[0]);
+		while (pos != (ssize_t)(std::string::npos))
+		{
+			buff.erase(pos, oldnew[0].length());
+			buff.insert(pos, oldnew[1]);
+			pos = buff.find(oldnew[0], pos);
+		}
+		output << buff;
+		if (!input.eof())
+			output << std::endl;
+	}
 }
